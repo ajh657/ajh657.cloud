03e6f6d5593144549584f08d9b8169190111d410
Arttu Höylä <arttu.hoyla@adafy.com>
2023-09-19 13:58:49 +0300

    WIP

--- Navidrome/Image-cropper.ps1
+++ Navidrome/Image-cropper.ps1
@@ -1,2 +1,8 @@
-magick mogrify -page -340+0 -background none -flatten *.png
-magick mogrify -crop 720:720 -gravity Center *.png
+if ($args.Length -gt 1) {
+    $item = Get-Item $args[1]
+
+    magick $item.FullName -crop $args[0] -gravity Center "$($item.DirectoryName)\$($item.BaseName)-cropped$($item.Extension)"
+}
+else {
+    magick mogrify -crop $args[0] -gravity Center *.png
+}
